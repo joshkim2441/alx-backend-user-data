6bf6f25928b192e04401f3eabe27f4dc67ec8d6b
joshkim2441 <joshkim2441@gmail.com>
2024-07-31 16:04:06 +0300

    personal data

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -2,9 +2,13 @@
 """ Returns filtered logs"""
 
 import re
+import logging
+import mysql.connector
 from typing import List
+from os import environ
 
-PII = {"name": "name", "email": "email", "phone": "phone",
+
+PII_FIELDS = {"name": "name", "email": "email", "phone": "phone",
        "ssn": "ssn", "password": "password"}
 
 
@@ -15,3 +19,43 @@ def filter_datum(fields: List[str], redaction: str,
         message = re.sub(f'{f}=.*?{separator}',
                          f'{f}={redaction}{separator})', message)
     return message
+
+
+def get_db() -> mysql.connector.connection.MySQLConnection:
+    """ Returns a connector object"""
+    username = environ.get('PERSONAL_DATA_DB_USERNAME', 'root')
+    password = environ.get('PERSONAL_DATA_DB_PASSWORD', '')
+    host = environ.get("PERSONAL_DATA_DB_HOST", 'localhost')
+    db_name = environ.get('PERSONAL_DATA_DB_NAME')
+    return mysql.connector.connection.MySQLConnection(user=username,
+                                                      password=password,
+                                   host=host, database=db_name)
+
+
+def get_logger() -> logging.Logger:
+    """ Returns a logger object"""
+    logger = logging.getLogger('user_data')
+    logger.setLevel(logging.INFO)
+    logger.propagate = False
+    handler = logging.StreamHandler()
+    handler.setFormatter(RedactingFormatter(list(PII_FIELDS.values())))
+    logger.addHandler(handler)
+    return logger
+
+
+class RedactingFormatter(logging.Formatter):
+    """ Redacting Formatter class
+        """
+
+    REDACTION = "***"
+    FORMAT = "[HOLBERTON] %(name)s %(levelname)s %(asctime)-15s: %(message)s"
+    SEPARATOR = ";"
+
+    def __init__(self, fields: List[str]):
+        super(RedactingFormatter, self).__init__(self.FORMAT)
+        self.fields = fields
+
+    def format(self, record: logging.LogRecord) -> str:
+        """Filters values in incoming log records using filter_datum"""
+        return filter_datum(self.fields, self.REDACTION,
+                            super().format(record), self.SEPARATOR)
