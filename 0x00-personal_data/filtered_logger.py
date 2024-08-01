03bf0e421fd02fe161599042c7995e2b14f5924f
joshkim2441 <joshkim2441@gmail.com>
2024-08-01 07:48:02 +0300

    correction of logger

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -39,9 +39,9 @@ def get_db() -> mysql.connector.connection.MySQLConnection:
     host = environ.get("PERSONAL_DATA_DB_HOST", "localhost")
     db_name = environ.get("PERSONAL_DATA_DB_NAME")
     cnx = mysql.connector.connection.MySQLConnection(user=username,
-                                                      password=password,
-                                                      host=host,
-                                                      database=db_name)
+                                                     password=password,
+                                                     host=host,
+                                                     database=db_name)
     return cnx
 
 
