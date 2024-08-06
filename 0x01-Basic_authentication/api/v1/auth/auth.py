bdbdf097b5dee4d92be112b82ead3514d4fc4518
joshkim2441 <joshkim2441@gmail.com>
2024-08-06 05:32:04 +0300

    authentication

---
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python3
+""" Module of Auth
+"""
+import re
+from api.v1.views import app_views
+from flask import request
+from typing import List, TypeVar
+from models.user import User
+
+
+class Auth:
+    """ Auth class
+    """
+    def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
+        """ Require Auth
+        """
+        if path is not None and excluded_paths is not None:
+            for exclusion_path in map(lambda x: x.strip(), excluded_paths):
+                pattern = ''
+                if exclusion_path[-1] == '*':
+                    pattern = '{}.*'.format(exclusion_path[0:-1])
+                elif exclusion_path[-1] == '/':
+                    pattern = '{}.*'.format(exclusion_path[0:-1])
+                else:
+                    pattern = '{}/*'.format(exclusion_path)
+                if re.match(pattern, path):
+                    return False
+        return True
+
+    def authorization_header(self, request=None) -> str:
+        """ Get authorization header field from request
+        """
+        if request is None:
+            return request.headders.get('Authorization', None)
+        return None
+
+    def current_user(self, request=None) -> TypeVar('User'):
+        """ Get current user from request
+        """
+        return None
